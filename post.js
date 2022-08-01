5ee6d4f5ce97ef45aef2495716d194290bec372e
Erwin Stone <erwinstone@pm.me>
2022-08-01 09:54:45 +0700

    move "multiparty" due to file order issue

--- post.js
+++ post.js
@@ -2,7 +2,7 @@
 
 const fp = require('fastify-plugin')
 const { parse } = require('querystring')
-const formidable = require('formidable')
+const multiparty = require('multiparty')
 
 function fastifyPost(fastify, options, next) {
   const bodyLimit = options.bodyLimit || fastify.initialConfig.bodyLimit
@@ -14,39 +14,47 @@ function fastifyPost(fastify, options, next) {
     }
   )
   fastify.addContentTypeParser('multipart', (_req, body, done) => {
-    const form = formidable({
-      multiples: true,
-      keepExtensions: true,
+    const form = new multiparty.Form({
       maxFieldsSize: bodyLimit,
-      maxFileSize: options.maxFileSize || 100 * 1024 * 1024 //100MB
+      maxFields: options.maxFields || 1000,
+      maxFilesSize: options.maxFilesSize || 100 * 1024 * 1024, //100MB
     })
     form.parse(body, (err, fields, files) => {
       if (err) {
         done(err)
       } else {
-        let jsonFiles = {}
-        if (files) {
-          for (const key of Object.keys(files)) {
-            if (Array.isArray(files[key])) {
-              jsonFiles[key] = []
-              for (const i of files[key]) {
-                const jsonData = i.toJSON()
-                jsonFiles[key].push(jsonData.size > 0 ? jsonData : null)
-              }
-            } else {
-              const jsonData = files[key].toJSON()
-              jsonFiles[key] = jsonData.size > 0 ? jsonData : null
-            }
+        for (const key of Object.keys(fields)) {
+          const val = fields[key]
+          if (val.length === 1) {
+            fields[key] = val[0]
           }
         }
-        done(null ,{ ...fields, ...jsonFiles })
+        for (const key of Object.keys(files)) {
+          const val = files[key]
+          if (val.length === 1) {
+            files[key] = fileFormat(val[0])
+          } else {
+            files[key] = val.map((i) => fileFormat(i))
+          }
+        }
+        done(null, { ...fields, ...files })
       }
     })
   })
   next()
 }
 
+function fileFormat(file) {
+  if (file.size === 0) return null
+  return {
+    fileName: file.originalFilename,
+    fileMime: file.headers['content-type'],
+    filePath: file.path,
+    fileSize: file.size,
+  }
+}
+
 module.exports = fp(fastifyPost, {
   fastify: '4.x',
-  name: 'fastify-post'
+  name: 'fastify-post',
 })
